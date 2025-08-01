d234052672b295d68f4fa7b9d5ce6df07493dd2a
Malachi Seerden <malachi.seerden@science.edu>
2025-08-01 16:17:43 -0500

    Typing

--- compatibility/compatibility.go
+++ compatibility/compatibility.go
@@ -1,8 +1,19 @@
 package compatibility
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 type Score struct {
+	User1      Analysis
+	User2      Analysis
+	Similarity float64
+}
+
+type Analysis struct {
+	Formality float64
+	Sentiment float64
+	Favorites []string
 }
 
 func Assess(u1, u2 discordgo.User) Score {
