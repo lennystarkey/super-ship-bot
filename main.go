ea7652eb7abd9dad649f90adecc3378fa734c5ea
Malachi Seerden <malachi.seerden@science.edu>
2025-08-01 00:28:11 -0500

    Temporarily parse from cmd line instead of using env vars

--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 )
 
 func main() {
-	dcToken, ok := os.LookupEnv("SUPERSHIP_TOKEN")
-	if !ok {
-		log.Fatal("must set environment variable SUPERSHIP_TOKEN to the discord bot token")
+	// temporarily parse the token from cmd line, for testing purposes
+	dcToken := ""
+	if len(os.Args) > 1 {
+		dcToken = os.Args[1]
+	} else {
+		log.Fatal("token not included as argument")
 	}
+	// dcToken, ok := os.LookupEnv("SUPERSHIP_TOKEN")
+	// if !ok {
+	// 	log.Fatal("must set environment variable SUPERSHIP_TOKEN to the discord bot token")
+	// }
+
 	// aiToken, ok := os.LookupEnv("AI_TOKEN")
 	// if !ok {
 	// 	log.Fatal("must set environment variable AI_TOKEN to the ai api token")
