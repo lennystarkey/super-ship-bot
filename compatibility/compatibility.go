169c8918b100776b5c12316c68610bb0fb8adf48
Malachi Seerden <malachi.seerden@science.edu>
2025-08-06 02:12:30 -0500

    Query hf inference for sentiment analysis

--- compatibility/compatibility.go
+++ compatibility/compatibility.go
@@ -1,6 +1,16 @@
 package compatibility
 
-type Score struct {
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Result struct {
 	User1      Analysis
 	User2      Analysis
 	Similarity float64
@@ -12,6 +22,56 @@ type Analysis struct {
 	Favorites []string
 }
 
-func Assess(u1, u2 string) Score {
-	return Score{}
+type apiResponse struct {
+	Label string  `json:"label"`
+	Score float64 `json:"score"`
+}
+
+var (
+	hfToken           = flag.String("hfToken", "", "token")
+	sentimentAnalysis = "https://router.huggingface.co/hf-inference/models/tabularisai/multilingual-sentiment-analysis"
+)
+
+func Assess(u1, u2 string) (Result, error) {
+	if len(*hfToken) == 0 {
+		return Result{}, errors.New("please provide Hugging Face Inference API token")
+	}
+
+	return Result{}, nil
+}
+
+// queries a map of scores from an hf inference
+func queryApi(url, input string) (map[string]float64, error) {
+	data, err := json.Marshal(map[string]string{"inputs": input})
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal payload: %v", err)
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *hfToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response: %v", err)
+	}
+
+	var apiRsp [][]apiResponse
+	if err := json.Unmarshal(body, &apiRsp); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %v", err)
+	}
+	m := make(map[string]float64)
+	for _, r := range apiRsp[0] {
+		m[r.Label] = r.Score
+	}
+	return m, nil
 }
---
+++ compatibility/compatibility_test.go
@@ -0,0 +1,23 @@
+package compatibility
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRequestScore(t *testing.T) {
+	content := []string{
+		"i love food! it tastes so wonderful. mmmm",
+		"food is not amazing, it disappointed me although i liked the alarm clock.",
+		"this food is absolutely terrible. everyone slapped me and i cried.",
+	}
+	for _, c := range content {
+		fmt.Println(c)
+		m, err := queryApi(sentimentAnalysis, c)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(m)
+	}
+
+}
